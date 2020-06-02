981a23746bf2a500e668491d0b13e0a032028b06
jcamilo20 <44034804+jcamilo20@users.noreply.github.com>
2020-06-02 00:46:37 -0500

    ACTUALIZACION

--- PARCIAL/htdocs/administradores/sentencias.php
+++ PARCIAL/htdocs/administradores/sentencias.php
@@ -5,11 +5,11 @@ require 'databaseMYSQL.php';
 
 // SENTENCIA DE SELECCION
 
-$seleccionar    = "SELECT PK_admin, nombre, apellido, email, Created, Updated FROM TBL_administradores";
-
 try {
             $conn = new PDO("mysql:host=$server;dbname=$database", $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $seleccionar    = "SELECT PK_admin, nombre, apellido, email, Created, Updated FROM TBL_administradores";
 
             foreach ($conn->query($seleccionar) as $row) {
                   echo "PK_admin: " . $row["PK_admin"] . " - nombre: " . $row["nombre"] . " - apellido: " . $row["apellido"] . " - email: " . $row["email"] . " - Created: " . $row["Created"] . " - Updated: " . $row["Updated"] . "<br>";
@@ -23,12 +23,13 @@ catch (PDOException $e) {
 /*
 // SENTENCIA DE INSERCIÓN
 
-$insertar = "INSERT INTO TBL_administradores (nombre, apellido, email, password, Created, Updated)
-VALUES ('John', 'Doe', 'john@example.com', 'hello123', '2020-05-26 04:09:11', '2020-05-27 04:09:11')";
 
 try {
             $conn = new PDO("mysql:host=$server;dbname=$database", $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $insertar = "INSERT INTO TBL_administradores (nombre, apellido, email, password, Created, Updated)
+            VALUES ('John', 'Doe', 'john@example.com', 'hello123', '2020-05-26 04:09:11', '2020-05-27 04:09:11')";
 
     // use exec() because no results are returned
     $conn->exec($insertar);
@@ -43,11 +44,11 @@ catch (PDOException $e) {
 /*
 // SENTENCIA DE ELIMINACIÓN 
 
-$eliminar = "DELETE FROM TBL_administradores WHERE PK_admin=7";
-
 try {
             $conn = new PDO("mysql:host=$server;dbname=$database", $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $eliminar = "DELETE FROM TBL_administradores WHERE PK_admin=7";
 
     // use exec() because no results are returned
     $conn->exec($eliminar);
@@ -62,11 +63,12 @@ catch (PDOException $e) {
 /*
 // SENTENCIA DE ACTUALIZACIÓN
 
-$actualizar = "UPDATE TBL_administradores SET apellido='Cambio', Updated='2020-06-01 04:09:11' WHERE PK_admin=2";
 
 try {
             $conn = new PDO("mysql:host=$server;dbname=$database", $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $actualizar = "UPDATE TBL_administradores SET apellido='Cambio', Updated='2020-06-01 04:09:11' WHERE PK_admin=2";
 
     // use exec() because no results are returned
     $conn->exec($actualizar);
@@ -83,11 +85,12 @@ catch (PDOException $e) {
 
 // PROCEDIMIENTO
 
-$procedimiento = "CALL `SP_Audita1B`()";
 
 try {
             $conn = new PDO("mysql:host=$server;dbname=$database", $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $procedimiento = "CALL `SP_Audita1B`()";
 
             foreach ($conn->query($procedimiento) as $row) {
                   echo "apellido: " . $row["apellido"] . " - email: " . $row["email"] . " - Updated: " . $row["Updated"] . " - Created: " . $row["Created"] . " - nombre: " . $row["nombre"] . " - PK_admin: " . $row["PK_admin"] . "<br>";
